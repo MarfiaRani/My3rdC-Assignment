e0eed05ba83ae171d78bacad104f654962a72dba
Marfia <marfiarani965@gmail.com>
2024-05-25 00:24:21 +0500

    Add project files.

---
+++ My3rdC# Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//creating  a class 
+public class ToolVehicle
+{
+  public int VehicleID;
+    public string? RegNo;
+    public string? Model;
+    public string? Brand;
+    public string? BasePrice;
+    public string? VehicleType;
+
+    static int TotalVehicles;
+
+
+}
+
